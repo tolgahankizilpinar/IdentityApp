a5b8bafcb5350dbc15482359c3edda0b4eae7d31
Your Name <tolgahan.kizilpinar@hotmail.com>
2025-07-23 22:03:13 +0300

    Hesap Onay linki

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -15,7 +15,7 @@ namespace IdentityApp.Controllers
         {
             _userManager = userManager;
             _roleManager = roleManager;
-            _signInManager = signInManager; 
+            _signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -34,6 +34,12 @@ namespace IdentityApp.Controllers
                 if (user != null)
                 {
                     await _signInManager.SignOutAsync();
+
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError("", "Hesabınızı onaylayınız");
+                        return View(model);
+                    }
 
                     var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, true);
 
@@ -62,6 +68,66 @@ namespace IdentityApp.Controllers
             }
 
             return View(model);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = model.UserName, Email = model.Email, FullName = model.FullName };
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var url = Url.Action("ConfirmEmail", "Account", new { user.Id, token });
+
+                    //email
+
+                    TempData["message"] = "Email hesabınızdaki onay mailini tıklayınız";
+                    return RedirectToAction("Login", "Account");
+                }
+
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string Id, string token)
+        {
+            if (Id == null || token == null)
+            {
+                TempData["message"] = "Geçersiz token bilgisi";
+                return View();
+            }
+
+            var user = await _userManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+
+                if (result.Succeeded)
+                {
+                    TempData["message"] = "Hesabınız onaylandı";
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+
+            TempData["message"] = "Kullanıcı bulunamadı";
+            return View();
         }
     }
 }
